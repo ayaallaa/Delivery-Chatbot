e8843d1ab71d855c4b80366df5a829c26c0d68ac
Hager Abdelazim <36241000+hagermohamed66@users.noreply.github.com>
2021-07-28 20:08:42 +0200

    Modify confirmation of time, entrance slot validation

--- Rasa_Chatbot/actions/actions.py
+++ Rasa_Chatbot/actions/actions.py
@@ -14,41 +14,7 @@ from rasa_sdk.events import SlotSet, EventType,UserUtteranceReverted
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 
-# class Actionlocation(Action):
 
-#     def name(self) -> Text:
-#         return "action_choose_location"
-
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-#         entities=tracker.latest_message['entities']
-#         print(entities)
-        
-#         # locations = {'Chappe':[], 'Curie':['E', 'D', 'B', 'C'],'Shannon':[], 
-#         #               'Pascal':[], 'Laplace':['East','West'],'Torricelli':[], 
-#         #               'Ritchie':[], 'Copernic':['G','H', 'EBC'], 'Bourseul':[],
-#         #               'Galilee':[], 'Montgomerie':[], 'Huygens':[], 
-#         #               'Newton':['F','B'] }
-        
-        
-        
-#         for e in entities:
-            
-#             if e['entity'] =='location':
-#                 location=e['value'].lower()
-                
-#             if location == 'curie' or location == 'laplace' or location == 'copernic':
-#                  entrance = ', please choose entrance'
-                
-#             message = 'Building is ' + location + entrance
-            
-#             print("Building: ",tracker.get_slot('location'))
-#             print(tracker.latest_message['text'])
-#         dispatcher.utter_message(text=message)
-
-#         return []
     
 
 class AppointmentInfoForm(Action):
@@ -65,10 +31,10 @@ class AppointmentInfoForm(Action):
         # if tracker.get_slot('confirm_time') == True:
         #       required_slots = ["confirm_time","location"]
         # else:
-        #    required_slots = ["confirm_exercise", "time","location"]
+        #     required_slots = ["confirm_exercise", "time","location"]
         
-        # required_slots = ["confirm_exercise", "time","location"]
-        required_slots = ["time","location"]
+        required_slots = ["confirm_exercise", "time","location", "entrance"]
+        # required_slots = ["time","location"]
         for slot_name in required_slots:
 
             if tracker.slots.get(slot_name) is None:
@@ -79,43 +45,7 @@ class AppointmentInfoForm(Action):
         # All slots are filled.
         return [SlotSet("requested_slot", None)]
     
-    # @staticmethod
-    # def required_slots(tracker):
-
-    #       if tracker.get_slot('confirm_time') == True:
-    #         return ["confirm_time", "location"]
-    #       else:
-    #         return ["confirm_exercise", "time",
-    #           "location"]
-        
-    # def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
-        
-    #       return {
-    #         "confirm_time": [
-    #             self.from_intent(intent="affirm", value=True),
-    #             self.from_intent(intent="deny", value=False)
-    #                  ]
-            # "time": [
-            #     self.from_entity(entity="time")
-            # ],
-            # "location": [
-            #     self.from_entity(entity="location")
-            # ]
-            # }
-     
-    #  def submit(
-    #     self,
-    #     dispatcher: CollectingDispatcher,
-    #     tracker: Tracker,
-    #     domain: Dict[Text, Any],
-    # ) -> List[Dict]:
-
-    #     confirm_time = tracker.get_slot("confirm_time")
-    #     time = tracker.get_slot("time")
-    #     location = tracker.get_slot("location")
-
-    #     dispatcher.utter_message("Thanks, your answers have been recorded!")
-    #     return []
+   
     
 class ActionSubmit(Action):
     def name(self) -> Text:
@@ -128,8 +58,10 @@ class ActionSubmit(Action):
         domain: "DomainDict",
     ) -> List[Dict[Text, Any]]:
         dispatcher.utter_message(response="utter_details_thanks",
+                                  Confirmed=tracker.get_slot("confirm_time"),
                                   Time=tracker.get_slot("time"),
-                                  Location=tracker.get_slot("location"))
+                                  Location=tracker.get_slot("location"),
+                                  Entrance= tracker.get_slot("entrance") )
         
 
 
@@ -137,7 +69,7 @@ class ValidateNameForm(FormValidationAction):
     def name(self) -> Text:
         return "validate_user_details_form"
 
-    def validate_location(
+    async def validate_location(
         self,
         slot_value: Any,
         dispatcher: CollectingDispatcher,
@@ -146,31 +78,51 @@ class ValidateNameForm(FormValidationAction):
     ) -> Dict[Text, Any]:
         """Validate `location` value."""
         
-        locations = {'chappe':[], 'curie':['E', 'D', 'B', 'C'],'shannon':[], 
+        self.locations = {'chappe':[], 'curie':['E', 'D', 'B', 'C'],'shannon':[], 
              'pascal':[], 'laplace':['East','West'],'torricelli':[], 
              'ritchie':[], 'copernic':['G','H', 'EBC'], 'bourseul':[],
              'galilee':[], 'montgomerie':[], 'huygens':[], 'newton':['F','B'] }
         
         # If the building isn't exist
         print(f"location given = {slot_value}")
-        if slot_value.lower() in locations:
-            return {"location": slot_value}
+     
+        if slot_value.lower() in self.locations:
+            if len(self.locations.get(slot_value.lower()))>0:
+              return{"location": slot_value}
+            else:
+              return {"entrance":"No entrance","location": slot_value}
         else:
+            # dispatcher.utter_message(image="https://i.imgur.com/nGF1K8f.jpg")
             dispatcher.utter_message(text="That's not valid Building. \nYou should choose one of the following: \n['Chappe', 'Curie', 'Shannon', 'Pascal', 'Laplace', 'Torricelli','Ritchie', 'Copernic', \n'Bourseul', 'Galilee', 'Montgomerie', 'Huygens', 'Newton'].")
             return {"location": None}
         
 
-    # async def validate_confirm_time(
-    #     self,
-    #     value: Text,
-    #     dispatcher: CollectingDispatcher,
-    #     tracker: Tracker,
-    #     domain: Dict[Text, Any],
-    # ) -> Dict[Text, Any]:
-    #     if value:
-    #         return {"confirm_time": True}
-    #     else:
-    #         return {"time": "None", "confirm_time": False }
+    async def validate_confirm_time(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        if value:
+            return {"time": "10 am","confirm_time": True}
+        else:
+            return {"confirm_time": False }
+        
+    def validate_entrance(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        
+        Entrances= self.locations.get(tracker.get_slot("location"))
+        if value in Entrances:
+            return {"entrance": value}
+        else:
+            dispatcher.utter_message(text="That's not valid Entrance. \nYou should choose one of the following: \n{Entrances}.")
+            return {"entrance": None}
         
         
         
