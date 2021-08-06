ab0c9468a6a95442cbcbe6501a77a1052c543d4b
Hager Abdelazim <36241000+hagermohamed66@users.noreply.github.com>
2021-08-06 03:58:40 +0200

    Add custom buttons for entrances

--- Rasa_Chatbot/actions/actions.py
+++ Rasa_Chatbot/actions/actions.py
@@ -85,14 +85,38 @@ class ValidateNameForm(FormValidationAction):
         
         # If the building isn't exist
         print(f"location given = {slot_value}")
-     
-        if slot_value.lower() in self.locations:
-            if len(self.locations.get(slot_value.lower()))>0:
+        
+        building = slot_value.lower()
+        
+        if building in self.locations:
+            
+            if len(self.locations.get(building))>0:
+              if building == 'curie':
+                    dispatcher.utter_message(buttons = [
+                    {"payload": "/Entrance{'entrance':'E'}", "title": "E","type": "postBack"},
+                    {"payload": "/Entrance{'entrance':'B'}", "title": "B","type": "postBack"},
+                    {"payload": "/Entrance{'entrance':'C'}", "title": "C","type": "postBack"},
+                    {"payload": "/Entrance{'entrance':'D'}", "title": "D","type": "postBack"},])
+              elif building == 'laplace':
+                  dispatcher.utter_message(buttons = [
+                    {"payload": "/Entrance{'entrance':'East'}", "title": "East","type": "postBack"},
+                    {"payload": "/Entrance{'entrance':'West'}", "title": "West","type": "postBack"},
+                    ])
+              elif building == 'copernic':
+                  dispatcher.utter_message(buttons = [
+                    {"payload": "/Entrance{'entrance':'G'}", "title": "G","type": "postBack"},
+                    {"payload": "/Entrance{'entrance':'H'}", "title": "H","type": "postBack"},
+                    {"payload": "/Entrance{'entrance':'EBC'}", "title": "EBC","type": "postBack"},
+                    ])
+              elif building == 'newton':
+                  dispatcher.utter_message(buttons = [
+                    {"payload": "/Entrance{'entrance':'F'}", "title": "F","type": "postBack"},
+                    {"payload": "/Entrance{'entrance':'B'}", "title": "B","type": "postBack"},
+                    ])
               return{"location": slot_value}
             else:
-              return {"entrance":"No entrance","location": slot_value}
+              return {"entrance":"no","location": slot_value}
         else:
-            # dispatcher.utter_message(image="https://i.imgur.com/nGF1K8f.jpg")
             dispatcher.utter_message(text="That's not valid Building. \nYou should choose one of the following: \n['Chappe', 'Curie', 'Shannon', 'Pascal', 'Laplace', 'Torricelli','Ritchie', 'Copernic', \n'Bourseul', 'Galilee', 'Montgomerie', 'Huygens', 'Newton'].")
             return {"location": None}
         
@@ -138,5 +162,42 @@ class ActionCustomFallback(Action):
         dispatcher.utter_message(template="utter_custom")
         
         return[UserUtteranceReverted()]  # to consider it a fallback action
+    
+
+# class ActionCustomEntrance(Action):
+
+#     def name(self) -> Text:
+#         return "utter_ask_entrance"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+#         building = tracker.get_slot("location")
+#         dispatcher.utter_message(text="Please choose entrance")
+#         if building.lower() == 'curie':
+#                    dispatcher.utter_message(buttons = [
+#                     {"payload": "/Entrance{'entrance':'E'}", "title": "E","type": "postBack"},
+#                     {"payload": "/Entrance{'entrance':'B'}", "title": "B","type": "postBack"},
+#                     {"payload": "/Entrance{'entrance':'C'}", "title": "C","type": "postBack"},
+#                     {"payload": "/Entrance{'entrance':'D'}", "title": "D","type": "postBack"},])
+#         elif building.lower() == 'laplace':
+#                   dispatcher.utter_message(buttons = [
+#                     {"payload": "/Entrance{'entrance':'East'}", "title": "East","type": "postBack"},
+#                     {"payload": "/Entrance{'entrance':'West'}", "title": "West","type": "postBack"},
+#                     ])
+#         elif building.lower() == 'copernic':
+#                   dispatcher.utter_message(buttons = [
+#                     {"payload": "/Entrance{'entrance':'G'}", "title": "G","type": "postBack"},
+#                     {"payload": "/Entrance{'entrance':'H'}", "title": "H","type": "postBack"},
+#                     {"payload": "/Entrance{'entrance':'EBC'}", "title": "EBC","type": "postBack"},
+#                     ])
+#         elif building.lower() == 'newton':
+#                   dispatcher.utter_message(buttons = [
+#                     {"payload": "/Entrance{'entrance':'F'}", "title": "F","type": "postBack"},
+#                     {"payload": "/Entrance{'entrance':'B'}", "title": "B","type": "postBack"},
+#                     ])
+        
+#         return[]  
 
    
